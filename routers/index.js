cc72e2a599134e928a478638faaca1d8f4a48b6c
Shift <notyet@notadomain.com>
2021-04-30 00:21:46 +0200

    rm of comment: success handler

--- routers/index.js
+++ routers/index.js
@@ -7,13 +7,4 @@ const apiRouter = new Router({ prefix: "/api" });
 apiRouter.get("/status", statusHandler);
 apiRouter.use("/post", dbActive, post.routes());
 
-//handle success
-// apiRouter.use((ctx, next) => {
-//   const { body, success = false } = ctx;
-//   if (!body && !success) {
-//     return next();
-//   }
-//   ctx.body = { ...body, success };
-// });
-
 export default apiRouter;
